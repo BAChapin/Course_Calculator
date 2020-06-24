bb050c2efbc7b7da8f4f8b0529df81ccfd3d0c89
BAChapin <brett.chapin89@gmail.com>
2020-06-24 00:17:40 -0400

    Removed many lines of code by implementing kotlinx.android.synthetic

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,17 +2,19 @@ package com.example.calculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import java.lang.NumberFormatException
 
-class MainActivity : AppCompatActivity() {
+import kotlinx.android.synthetic.main.activity_main.*
 
-    private lateinit var result: EditText
-    private lateinit var newNumber: EditText
-    private val displayOperation by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation) }
+private const val TAG = "MainActivity"
+private const val OPERATOR = "Operator"
+private const val OPERAND = "Operand"
+private const val OPERAND_STORED = "OperandStored"
+
+class MainActivity : AppCompatActivity() {
 
     // Variables for operands and type of calculations
     private var operand1: Double? = null
@@ -21,29 +23,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        result = findViewById(R.id.result)
-        newNumber = findViewById(R.id.newNumber)
-
-        // Data Input Buttons
-        val button0: Button = findViewById(R.id.button0)
-        val button1: Button = findViewById(R.id.button1)
-        val button2: Button = findViewById(R.id.button2)
-        val button3: Button = findViewById(R.id.button3)
-        val button4: Button = findViewById(R.id.button4)
-        val button5: Button = findViewById(R.id.button5)
-        val button6: Button = findViewById(R.id.button6)
-        val button7: Button = findViewById(R.id.button7)
-        val button8: Button = findViewById(R.id.button8)
-        val button9: Button = findViewById(R.id.button9)
-        val buttonDot: Button = findViewById(R.id.buttonDot)
-
-        // Operation Buttons
-        val buttonEqual: Button = findViewById(R.id.buttonEquals)
-        val buttonMutliply: Button = findViewById(R.id.buttonMultiply)
-        val buttonDivide: Button = findViewById(R.id.buttonDivide)
-        val buttonAdd: Button = findViewById(R.id.buttonPlus)
-        val buttonMinus: Button = findViewById(R.id.buttonMinus)
 
         val listener = View.OnClickListener { v ->
             val b = v as Button
@@ -71,13 +50,13 @@ class MainActivity : AppCompatActivity() {
                 newNumber.setText("")
             }
             pendingOperation = op
-            displayOperation.text = pendingOperation
+            operation.text = pendingOperation
         }
 
-        buttonEqual.setOnClickListener(opListener)
-        buttonMutliply.setOnClickListener(opListener)
+        buttonEquals.setOnClickListener(opListener)
+        buttonMultiply.setOnClickListener(opListener)
         buttonDivide.setOnClickListener(opListener)
-        buttonAdd.setOnClickListener(opListener)
+        buttonPlus.setOnClickListener(opListener)
         buttonMinus.setOnClickListener(opListener)
 
     }
@@ -105,5 +84,28 @@ class MainActivity : AppCompatActivity() {
         }
         result.setText(operand1.toString())
         newNumber.setText("")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG,"onSaveInstanceState(outState:) called")
+        super.onSaveInstanceState(outState)
+        outState.putString(OPERATOR, pendingOperation)
+        if (operand1 != null) {
+            outState.putDouble(OPERAND, operand1!!)
+            outState.putBoolean(OPERAND_STORED, true)
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG, "onRestoreInstanceState(savedInstanceState:) called")
+        super.onRestoreInstanceState(savedInstanceState)
+        pendingOperation = savedInstanceState.getString(OPERATOR, "=")
+        operation.text = pendingOperation
+        operand1 = if (savedInstanceState.getBoolean(OPERAND_STORED, false)) {
+            savedInstanceState.getDouble(OPERAND)
+        } else {
+            null
+        }
+        operand1 = savedInstanceState.getDouble(OPERAND)
     }
 }
